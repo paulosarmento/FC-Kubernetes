1eef089ca0753281634fe710bb45220ad93d107e
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2023-11-30 14:24:26 -0300

    docs: :memo: Criando e configurando HPA

--- server.go
+++ server.go
@@ -39,14 +39,14 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "My Family: %s.", string(data))
 }
 func Healthz(w http.ResponseWriter, r *http.Request) {
-	duration := time.Since(startedAt)
-
-	if duration.Seconds() < 10 || duration.Seconds() > 30 {
-		w.WriteHeader(500)
-		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
-	} else {
-		w.WriteHeader(200)
-		w.Write([]byte("ok"))
-	}
+  duration := time.Since(startedAt)
+ 
+  if duration.Seconds() < 10 {
+    w.WriteHeader(500)
+    w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+  } else {
+    w.WriteHeader(200)
+    w.Write([]byte("ok"))
+  }
 }
 
