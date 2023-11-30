d046d2d0fd0f4056c6d9c3408266b12be5c46274
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2023-11-30 07:32:12 -0300

    docs: :memo: Secrets e variáveis de ambiente

--- server.go
+++ server.go
@@ -9,23 +9,29 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
 }
 
-func ConfigMap(w http.ResponseWriter, r *http.Request) {
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s. Password: %s.", user, password)
+}
 
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
 	data, err:= ioutil.ReadFile("myfamily/family.txt")
 	if err != nil {
 		log.Fatalf("Error reading file: ", err)
 	}
 	fmt.Fprintf(w, "My Family: %s.", string(data))
 }
+
